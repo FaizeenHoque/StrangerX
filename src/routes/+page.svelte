1e58d223535c4527b891c6f9a3281a9d94034cfe
Faizeen Hoque <haquefaizeen11@gmail.com>
2026-06-22 15:38:00 +0600

    setup, navbar, hero

---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<div class="h-screen flex flex-col">
+    <div class="w-screen border-b-2 border-accent-primary p-4">
+        <h1 class="font-mono-chrome font-thin text-4xl">
+            STRANGER<span class="text-accent-primary">X</span>_
+        </h1>
+    </div>
+
+    <div class="flex-1">
+        <div class="h-full border-r-2 border-accent-primary w-[50%] flex flex-col justify-center items-center">
+            <div class="pt-20">
+                <p class="text-accent-primary tracking-hacker-micro text-[15px]">
+                    > ANONYMOUS CHAT PROTOCOL
+                </p>
+
+                <h1 class="font-mono-chrome font-extrabold text-7xl pt-4 pb-5 text-center">
+                    TALK TO A <br/>
+                    <span class="terminal-cursor text-accent-primary">STRANGER_</span>
+                </h1>
+
+                <ul class="text-terminal-dim">
+                    <li class="p-[0.6px]">> ENCRYPTED CONNECTION</li>
+                    <li class="p-[0.6px]">> NO LOGS NO TRACES</li>
+                    <li class="p-[0.6px]">> RANDOM MATCH WORLD WIDE</li>
+                    <li class="p-[0.6px]">> DISCONNECT <span class="text-accent-primary">ANYTIME</span></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
